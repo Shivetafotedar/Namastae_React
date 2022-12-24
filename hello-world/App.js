92a560cc9a3b8811c3d9b4845014bb242e079301
shiveta <shiveta@sflhub.com>
2022-12-25 00:01:22 +0530

    Assignment-1 paritially compeleted

---
+++ hello-world/App.js
@@ -0,0 +1,5 @@
+const root = ReactDOM.createRoot(document.getElementById("root"))
+ const heading = React.createElement("h1" ,{}, "Namastae React")
+const heading2 =React.createElement("h2",{},"React")
+ root.render([heading,heading2])
+ 
