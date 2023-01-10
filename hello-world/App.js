19e7fe014e55b5d872b9f744d19ca1c39afab897
Shivetafotedar <shivetafotedar5151@gmail.com>
2023-01-10 21:06:41 +0530

    work done

--- hello-world/App.js
+++
@@ -1,5 +0,0 @@
-const root = ReactDOM.createRoot(document.getElementById("root"))
- const heading = React.createElement("h1" ,{}, "Namastae React")
-const heading2 =React.createElement("h2",{},"React")
- root.render([heading,heading2])
- 
